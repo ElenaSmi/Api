4dd2f99bb1aab6dcfc921cd3bdbd2146545f3e11
Lena Smirnov <Elena@Lenas-MacBook-Pro.local>
2015-08-10 21:38:38 -0700

    Permissions tests added

--- api_framework/src/api_utils.py
+++ api_framework/src/api_utils.py
@@ -4,6 +4,7 @@ import os
 import requests
 import json
 import time
+import sys
 
 class Config:
     def __init__(self):
@@ -12,13 +13,15 @@ class Config:
             self.parser.read('config.ini')
         else:
             print('No config.ini found under root folder.')
+            sys.exit()
         self.domain = self.parser.get('Server', 'domain')
         self.admin_login = self.parser.get('Server', 'admin')
         self.password = self.parser.get('Server', 'password')
         self.test_path = self.parser.get('Server', 'testpath')
+        self.puser = self.parser.get('Server', 'puser')
 
 
-class Response:#response alwayse has header and status code
+class Response:#response always has header and status code
     def __init__(self):
         self.http_code = 0
         self.json = dict()
@@ -57,15 +60,14 @@ class Calls:
         data = dict()
         data['action'] = 'add_folder'
 
-        data = json.dumps(data)# translates from dict to json
+        data = json.dumps(data)# translates from dict to json; loads translate from json to dict
 
         r = requests.request(
             url = url,
             auth = (username, password),
             headers = headers,
             data = data,
-            method = method
-        )
+            method = method)
 
         try:
             json_resp = json.loads(r.content)
@@ -126,6 +128,68 @@ class Calls:
         response.http_code = r.status_code
         response.json = r.json
         response.headers = r.headers
+        return response
+
+    def set_perms(self, name, permission, user, domain=None, username=None, password=None,
+                  content_type=None, accept=None, method=None, test_path=None):
+        '''
+        curl -u<username>:<password> -H "Content-Type: application/json" "https://istepanko25.qa-egnyte.com/public-api
+        /v1/perms/folder/Shared/smoke_test/dynamic_name_143906369545/testname4853666" -d '{"users": ["atest"],
+         "permission": "Viewer"}' -X POST
+
+        '''
+        if domain is None:
+            domain = self.config.domain
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+        if method is None:
+            method = 'POST'
+        if accept is None:
+            accept = 'application/json'
+        if content_type is None:
+            content_type = 'application/json'
+        if test_path is None:
+            test_path = self.config.test_path
+
+        endpoint = '/public-api/v1/perms/folder'
+        url = domain + endpoint + test_path + name
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        data = dict()
+        data['permission'] = permission
+        data['users'] = list()
+        data['users'].append(user)
+        data = json.dumps(data)
+        # Request method returning server response to r
+        r = requests.request(
+            url=url,
+            auth=(username, password),
+            headers=headers,
+            data=data,
+            method=method
+        )
+
+        # Fist: Trying to parse json and convert it to Pythonic dict.
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            # If parsing failed then check, maybe method was 'OPTIONS'
+            if method == 'OPTIONS':
+                json_resp = r.content
+            # And finally return no_json string to json_resp variable
+            else:
+                json_resp = self.no_json
+        # Putting processed r.content into r.json
+        r.json = json_resp
+        # Creating object of class Response()
+        response = Response()
+        response.http_code = r.status_code
+        response.body = r.json
+        response.headers = r.headers
+        print('\n' + str(response.http_code))
         return response
 
     @staticmethod
--- api_framework/tests/testCreateFolder.py
+++ api_framework/tests/testCreateFolder.py
@@ -1,14 +1,18 @@
 __author__ = 'Elena'
 
 from api_utils import Calls
+from api_utils import Config
 from unittest import TestCase
 import httplib
+
+
 
 class TestClass(TestCase):
 
     @classmethod
     def setUpClass(cls): #cls - class is class method
         cls.calls = Calls()
+        cls.config = Config()
 
     def test_create_folder_positive(self):
         folder = self.calls.gen_random_name()
@@ -56,5 +60,112 @@ class TestClass(TestCase):
         assert resp.json['inputErrors']['credentials'][0]['msg'] == 'This request is unauthenticated. Please provide' \
                                                               ' credentials and try again.'
 
+    def test_delete_folder_wrong_accept_name(self):
+        folder_name = self.calls.gen_random_name()
+        resp = self.calls.delete_folder(folder_name, accept= 'application/xml')
+        assert resp.http_code == httplib.NOT_ACCEPTABLE
+
+    def test_create_and_delete_100_folders(self):
+        folder_name = self.calls.gen_random_name()
+        for i in range(100):
+            resp = self.calls.create_folder(folder_name + str(i))
+            assert resp.http_code == httplib.CREATED
+        for i in range(100):
+            resp = self.calls.delete_folder(folder_name + str(i))
+            assert resp.http_code == httplib.OK
 
 
+    def test_perms_full(self):
+        #Create folder with Full permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Full')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.create_folder(folder_name, username=self.config.puser, test_path='%s/%s/' %
+                                                                                (self.config.test_path, folder_name))
+        assert resp.http_code == httplib.CREATED
+
+    def test_perms_viewer(self):
+        #Create folder with Viewer permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Viewer')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.create_folder(folder_name, username=self.config.puser, test_path='%s/%s/' %
+                                                                                (self.config.test_path, folder_name))
+        assert resp.http_code == httplib.FORBIDDEN
+
+    def test_perms_editor(self):
+        #Create folder with Editor permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Editor')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.create_folder(folder_name, username=self.config.puser, test_path='%s/%s/' %
+                                                                                (self.config.test_path, folder_name))
+        assert resp.http_code == httplib.CREATED
+
+    def test_perms_none(self):
+        #Create folder with None permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='None')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.create_folder(folder_name, username=self.config.puser, test_path='%s/%s/' %
+                                                                                (self.config.test_path, folder_name))
+        assert resp.http_code == httplib.FORBIDDEN
+
+    def test_perms_owner(self):
+        #Create folder with Owner permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Owner')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.create_folder(folder_name, username=self.config.puser, test_path='%s/%s/' %
+                                                                                (self.config.test_path, folder_name))
+        assert resp.http_code == httplib.CREATED
+
+    def test_delete_folder_none(self):
+        #Delete folder None permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='None')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.delete_folder(folder_name, username=self.config.puser)
+        assert resp.http_code == httplib.FORBIDDEN
+
+    def test_delete_folder_full(self):
+        #Delete folder Full permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Full')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.delete_folder(folder_name, username=self.config.puser)
+        assert resp.http_code == httplib.OK
+
+    def test_delete_folder_viewer(self):
+        #Delete folder Viewer permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Viewer')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.delete_folder(folder_name, username=self.config.puser)
+        assert resp.http_code == httplib.FORBIDDEN
+
+    def test_delete_folder_editor(self):
+        #Delete folder Editor permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Editor')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.delete_folder(folder_name, username=self.config.puser)
+        assert resp.http_code == httplib.FORBIDDEN
+
+    def test_delete_folder_owner(self):
+        #Delete folder Owner permissions
+        folder_name = self.calls.gen_random_name()
+        self.calls.create_folder(folder_name)
+        resp = self.calls.set_perms(folder_name, user=self.config.puser, permission='Owner')
+        assert resp.http_code == httplib.OK
+        resp = self.calls.delete_folder(folder_name, username=self.config.puser)
+        assert resp.http_code == httplib.OK
